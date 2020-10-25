99f26fd2f27072a8d8e75c932747859c30e5226b
Jim Browning <jamalebrowning@gmail.com>
2020-10-24 22:38:31 -0500

    ch15

--- animals/barreleye.py
+++ animals/barreleye.py
@@ -1,17 +1,25 @@
 from datetime import date
 
 class Barreleye():
-    def __init__(self, name, species, shift, food):
+    def __init__(self, name, species, shift, food, chip_number):
         self.name = name
         self.species = species
         self.shift = shift
         self.date_added = date.today()
         self.food = food
         self.swimming = True
+        self.__chip_number = chip_number
+
+    @property
+    def chip_number(self):
+        return self.__chip_number   
+
+    @chip_number.setter
+    def chip_number(self, number):
+      pass 
     
     def __str__(self):
         return f"{self.name} is a {self.species}"
 
     def feed(self):
         return f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}'
-
--- attractions/petting_zoo.py
+++ attractions/petting_zoo.py
@@ -6,3 +6,7 @@ class Petting_Zoo:
 
     def add_animals(self, animal):
         self.animals.append(animal)
+
+    @property
+    def last_critter_added(self):
+      return f'{self.animals[-1].name} was the last critter added to {self.attraction_name}'
